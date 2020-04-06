f9eabf0f564db94e3b63596be8c3b71b6984f4df
Henrik <henrik90@hotmail.dk>
2020-04-06 12:07:42 +0200

    Init test commit

---
+++ ConsoleForTesting/ConsoleForTesting/Program.cs
@@ -0,0 +1,19 @@
+﻿using ChecksumHandler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleForTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yeet");
+            ChecksumTool.HelloWorld();
+            Console.ReadKey();
+        }
+    }
+}
