de68752ccbaeb092f4c928b8e1e787d230b08bc4
Henrik <henrik90@hotmail.dk>
2020-05-01 11:24:41 +0200

    Circular dependency fix

--- DatabaseREST/Controllers/AccountsController.cs
+++ DatabaseREST/Controllers/AccountsController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using DatabaseREST.DBModels;
 using DatabaseREST.Models;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
