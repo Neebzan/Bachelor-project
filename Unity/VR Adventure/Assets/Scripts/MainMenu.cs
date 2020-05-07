ff27f4fd69f3c79b90477d074f03390e1c71787d
Esben Dalsgard <esbenjd@gmail.com>
2020-05-07 15:25:56 +0200

    Working on main scene

---
+++ Unity/VR Adventure/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Canvas))]
+public class MainMenu : MonoBehaviour
+{
+    private Canvas _canvas;
+
+    private void Awake () {
+        _canvas = GetComponent<Canvas>();
+    }
+
+    public void PlayGame () {
+        Debug.Log("Play game pressed");
+    }
+}
