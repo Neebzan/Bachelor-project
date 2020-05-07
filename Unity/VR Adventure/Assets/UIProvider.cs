faa0ce6e0a145f50db6fc1f41bd629cedd8a2fdc
Esben Dalsgard <esbenjd@gmail.com>
2020-05-07 11:03:59 +0200

    Added unity UI interaction

---
+++ Unity/VR Adventure/Assets/UIProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class UIProvider : MonoBehaviour {
+    public List<XRDirectInteractor> DirectInteractors = null;
+    public List<XRRayInteractor> RayInteractors = null;
+    public List<XRController> Controllers = null;
+    public bool StartInMenu = false;
+    public GameObject UI;
+
+    private bool _menuOpened = false;
+    private bool _isPressingButton = false;
+    private CharacterController _characterController = null;
+
+    void Start () {
+        SetMenu(StartInMenu);
+
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void Update () {
+        CheckInput();
+    }
+
+    private void CheckInput () {
+        foreach (XRController controller in Controllers) {
+            if (controller.enableInputActions)  
+                CheckForMenuOpen(controller.inputDevice);
+            
+        }
+    }
+
+    private void CheckForMenuOpen (InputDevice inputDevice) {
+        if (inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonPressed)) {
+            if (!_isPressingButton && primaryButtonPressed) {
+                _isPressingButton = true;
+                Debug.Log("Primary button pressed");
+                SetMenu(!_menuOpened);
+            }
+            else if (_isPressingButton && !primaryButtonPressed) {
+                _isPressingButton = false;
+            }
+        }
+    }
+
+    private void SetMenu (bool value) {
+        _menuOpened = value;
+        UI.SetActive(value);
+        foreach (var interactor in DirectInteractors) {
+            interactor.gameObject.SetActive(!value);
+        }
+        foreach (var interactor in RayInteractors) {
+            interactor.gameObject.SetActive(value);
+        }
+    }
+}
