792085b6a106181adc1c4de06cc873d55e7cf2c6
Esben Dalsgard <esbenjd@gmail.com>
2020-05-13 17:05:49 +0200

    Working on spells

--- Unity/VR Adventure/Assets/Scripts/SpellCaster.cs
+++ Unity/VR Adventure/Assets/Scripts/SpellCaster.cs
@@ -13,6 +13,8 @@ public class SpellCaster : MonoBehaviour {
     public HandPresence LeftController;
     public HandPresence RightController;
 
+    public SpellController LeftSpellController;
+    public SpellController RightSpellController;
 
     private bool _isPressingRight = false;
     private bool _isPressingLeft = false;
@@ -24,9 +26,7 @@ public class SpellCaster : MonoBehaviour {
     private Spell _currentSpell = Spell.None;
 
 
-    private float _forcePushPower = 100.0f;
-    private float _forcePushChargeupTime = 1.0f;
-    private float _forcePushCharge = 0.0f;
+
 
     private Vector3 _pointBetween {
         get {
@@ -41,108 +41,110 @@ public class SpellCaster : MonoBehaviour {
 
     void Update () {
         CheckForSpellcast();
-        if (castingSpell) {
-            switch (_currentSpell) {
+        //if (castingSpell) {
+        //    switch (_currentSpell) {
 
-                case Spell.Fireball:
-                    _castFireball.transform.position = _pointBetween;
-                    float distance = Vector3.Distance(RightController.Hand.IndexFinger.position, LeftController.Hand.IndexFinger.position);
-                    _castFireball.Size = distance - .01f;
+        //        case Spell.Fireball:
+        //            _castFireball.transform.position = _pointBetween;
+        //            float distance = Vector3.Distance(RightController.Hand.IndexFinger.position, LeftController.Hand.IndexFinger.position);
+        //            _castFireball.Size = distance - .01f;
 
-                    break;
-                case Spell.ForcePush:
-                    ChargeForcePush();
-                    break;
-                default:
-                    break;
-            }
-        }
+        //            break;
+        //        case Spell.ForcePush:
+        //            ChargeForcePush();
+        //            break;
+        //        default:
+        //            break;
+        //    }
+        //}
 
-        if (_forcePushCharge > 0 && _currentSpell != Spell.ForcePush) {
-            DechargeForcePush();
-        }
+        //if (_forcePushCharge > 0 && _currentSpell != Spell.ForcePush) {
+        //    DechargeForcePush();
+        //}
     }
 
+
+    bool CheckHandUsingForce (HandPresence hand) {
+        if (hand.SecondaryButtonPressed)
+            return true;
+        
+        return false;
+    }
 
     private void CheckForSpellcast () {
-        if (!castingSpell) {
-            // Fireball - Pinch with hands, draw hands out to form fireball
-            if (RightController.CurrentGesture == HandGesture.Pinch && LeftController.CurrentGesture == HandGesture.Pinch &&
-                Vector3.Distance(RightController.Hand.IndexFinger.position, LeftController.Hand.IndexFinger.position) < .1f) {
-                castingSpell = true;
-                _currentSpell = Spell.Fireball;
-                CastFireball();
-            }
+        RightSpellController.UsingForce = CheckHandUsingForce(RightController);
+        LeftSpellController.UsingForce = CheckHandUsingForce(LeftController);
 
-            // Force Push - One, or both, hands flat, some button down, apply movement velocity
-            else if (RightController.Hand?.SkinnedMeshRenderer?.material != null) {
-                if (RightController.CurrentGesture == HandGesture.Open) {
-                    if (RightController.SecondaryButtonPressed) {
-                        castingSpell = true;
-                        _currentSpell = Spell.ForcePush;
-                    }
-                }
-            }
+        //if (!castingSpell) {
 
-            // Shield - Both hands flat, some button down, draw a square to form shield
-            if (RightController.CurrentGesture == HandGesture.Open && LeftController.CurrentGesture == HandGesture.Open) {
 
-            }
+
+
+
+        //    // Fireball - Pinch with hands, draw hands out to form fireball
+        //    if (RightController.CurrentGesture == HandGesture.Pinch && LeftController.CurrentGesture == HandGesture.Pinch &&
+        //        Vector3.Distance(RightController.Hand.IndexFinger.position, LeftController.Hand.IndexFinger.position) < .1f) {
+        //        castingSpell = true;
+        //        _currentSpell = Spell.Fireball;
+        //        CastFireball();
+        //    }
+            //// Force Push - One, or both, hands flat, some button down, apply movement velocity
+            //else if (RightController.Hand?.SkinnedMeshRenderer?.material != null) {
+            //    if (RightController.CurrentGesture == HandGesture.Open) {
+            //        if (RightController.SecondaryButtonPressed) {
+            //            castingSpell = true;
+            //            _currentSpell = Spell.ForcePush;
+            //        }
+            //    }
+            //}
+
+            //// Shield - Both hands flat, some button down, draw a square to form shield
+            //if (RightController.CurrentGesture == HandGesture.Open && LeftController.CurrentGesture == HandGesture.Open) {
+
+            //}
+        //}
+
+        //else if (castingSpell) {
+        //    if (_currentSpell == Spell.Fireball && (RightController.CurrentGesture != HandGesture.Pinch || LeftController.CurrentGesture != HandGesture.Pinch)) {
+        //        castingSpell = false;
+        //        _currentSpell = Spell.None;
+        //        _castFireball.Create();
+        //        _castFireball = null;
+        //    }
+
+            //if (_currentSpell == Spell.ForcePush && (!RightController.SecondaryButtonPressed || RightController.CurrentGesture != HandGesture.Open)) {
+            //    if (RightController.Velocity.magnitude > 1.0f) {
+            //        LayerMask mask = LayerMask.GetMask("TriggerCollider");
+            //        Collider [ ] collisions = Physics.OverlapSphere(transform.position, 5.0f, mask);
+            //        if (collisions.Length > 0) {
+            //            for (int i = 0; i < collisions.Length; i++) {
+            //                float angle = Vector3.Angle(-RightController.transform.up, (collisions [ i ].transform.position - RightController.transform.position).normalized);
+            //                Debug.Log("Angle = " + angle.ToString());
+
+            //                if (angle < 90) {
+            //                    Fireball fireball = collisions [ i ].GetComponent<Fireball>();
+            //                    if (fireball != null) {
+            //                        if (fireball.Active) {
+            //                            fireball.ApplyForce(RightController.Velocity * _forcePushPower * _forcePushCharge);
+            //                            //Get hit
+            //                        }
+            //                    }
+            //                }
+            //            }
+            //        }
+            //    }
+
+            //    _currentSpell = Spell.None;
+            //    castingSpell = false;
+            //}
         }
+    
 
-        else if (castingSpell) {
-            if (_currentSpell == Spell.Fireball && (RightController.CurrentGesture != HandGesture.Pinch || LeftController.CurrentGesture != HandGesture.Pinch)) {
-                castingSpell = false;
-                _currentSpell = Spell.None;
-                _castFireball.Create();
-                _castFireball = null;
-            }
 
-            if (_currentSpell == Spell.ForcePush && (!RightController.SecondaryButtonPressed || RightController.CurrentGesture != HandGesture.Open)) {
-                if (RightController.Velocity.magnitude > 1.0f) {
-                    LayerMask mask = LayerMask.GetMask("TriggerCollider");
-                    Collider [ ] collisions = Physics.OverlapSphere(transform.position, 5.0f, mask);
-                    if (collisions.Length > 0) {
-                        for (int i = 0; i < collisions.Length; i++) {
-                            float angle = Vector3.Angle(-RightController.transform.up, (collisions [ i ].transform.position - RightController.transform.position).normalized);
-                            Debug.Log("Angle = " + angle.ToString());
 
-                            if (angle < 90) {
-                                Fireball fireball = collisions [ i ].GetComponent<Fireball>();
-                                if (fireball != null) {
-                                    if (fireball.Active) {
-                                        fireball.ApplyForce(RightController.Velocity * _forcePushPower * _forcePushCharge);
-                                        //Get hit
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-
-                _currentSpell = Spell.None;
-                castingSpell = false;
-            }
-        }
-    }
-
-    private void ChargeForcePush () {
-        if (_forcePushCharge < _forcePushChargeupTime) {
-            Mathf.Clamp(_forcePushCharge += Time.deltaTime, 0, _forcePushChargeupTime);
-            float percentageCharged = _forcePushCharge / _forcePushChargeupTime;
-            RightController.Hand.SkinnedMeshRenderer.material.SetFloat("Vector1_654486F", percentageCharged);
-        }
-    }
-
-    private void DechargeForcePush () {
-        Mathf.Clamp(_forcePushCharge -= Time.deltaTime, 0, _forcePushChargeupTime);
-        float percentageCharged = _forcePushCharge / _forcePushChargeupTime;
-        RightController.Hand.SkinnedMeshRenderer.material.SetFloat("Vector1_654486F", percentageCharged);
-    }
-
-    private void CastFireball () {
-        GameObject instans = Instantiate(FireballPrefab);
-        instans.transform.position = _pointBetween;
-        _castFireball = instans.GetComponent<Fireball>();
-    }
+    //private void CastFireball () {
+    //    GameObject instans = Instantiate(FireballPrefab);
+    //    instans.transform.position = _pointBetween;
+    //    _castFireball = instans.GetComponent<Fireball>();
+    //}
 }
---
+++ Unity/VR Adventure/Assets/SpellController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellController : MonoBehaviour
+{
+    [HideInInspector]
+    public float ForceChargeupMax = 1.0f;
+    [HideInInspector]
+    public float ForceCharge = 0.0f;
+    [HideInInspector]
+    public float ForcePercentageCharged = 0.0f;
+    [HideInInspector]
+    public bool UsingForce = false;
+    
+    private Material _handMaterial;
+    private SphereCollider _collider;
+
+    private void Start () {
+        _handMaterial = GetComponent<Hand>().SkinnedMeshRenderer.material;
+        _collider = GetComponent<SphereCollider>();
+    }
+
+    private void Update () {
+        if (UsingForce) {
+            if (ForceCharge < 1.0f) {
+                ChargeForce();
+            }
+        }
+        else {
+            if (ForceCharge > 0.0f) {
+                DechargeForce();
+            }
+        }
+    }
+
+    public void ChargeForce () {
+        if (ForceCharge < ForceChargeupMax) {
+            ForceCharge += Time.deltaTime;
+            ForceCharge = Mathf.Clamp(ForceCharge, 0, ForceChargeupMax);
+            ForcePercentageCharged = ForceCharge / ForceChargeupMax;
+            _handMaterial.SetFloat("Vector1_654486F", ForcePercentageCharged);
+            Debug.Log(ForcePercentageCharged);
+        }
+    }
+
+    public void DechargeForce () {
+        ForceCharge -= Time.deltaTime;
+        ForceCharge = Mathf.Clamp(ForceCharge, 0, ForceChargeupMax);
+        ForcePercentageCharged = ForceCharge / ForceChargeupMax;
+        _handMaterial.SetFloat("Vector1_654486F", ForcePercentageCharged);
+    }
+
+    //private void OnTriggerEnter (Collider other) {
+    //    if (other.gameObject.layer) {
+
+    //    }
+    //}
+
+}
