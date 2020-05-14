16137143ae84177a90313e82b4104a58afb2eb0e
Esben Dalsgard <esbenjd@gmail.com>
2020-05-14 15:01:14 +0200

    Added emulated fireball prefab

---
+++ Unity/VR Adventure/Assets/EmulatedFireball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmulatedFireball : MonoBehaviour
+{
+    private Material _material;
+    private readonly string _sizeProperty = "Vector1_EE3E2A7D";
+    private float _size;
+
+    void Awake()
+    {
+        _material = GetComponent<MeshRenderer>().material;
+    }
+
+    public void Emulate (Vector3 position, float size) {
+        transform.position = position;
+        _size = size;
+        _material.SetFloat(_sizeProperty, _size);
+    }
+}
