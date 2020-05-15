cb89fcbb7776150902b926229cd11468e70da2cd
Esben Dalsgard <esbenjd@gmail.com>
2020-05-15 12:46:54 +0200

    Fixed emulated fireball spawn size

--- Unity/VR Adventure/Assets/EmulatedFireball.cs
+++ Unity/VR Adventure/Assets/EmulatedFireball.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class EmulatedFireball : MonoBehaviour {
     private Material _material;
     private readonly string _sizeProperty = "Vector1_EE3E2A7D";
-    private float _size;
+    private float _size = 0.0f;
     public int ID { get; private set; }
 
     void Awake () {
         _material = GetComponent<MeshRenderer>().material;
+        _material.SetFloat(_sizeProperty, _size);
     }
 
     public void Init (int id, float size) {
