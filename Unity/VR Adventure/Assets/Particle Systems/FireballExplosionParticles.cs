335d82140d82a129be1594d733df74bb673a32ad
Esben Dalsgard <esbenjd@gmail.com>
2020-05-15 17:39:57 +0200

    Added auto-destroy particle system after play

---
+++ Unity/VR Adventure/Assets/Particle Systems/FireballExplosionParticles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class FireballExplosionParticles : MonoBehaviour
+{
+    ParticleSystem particleSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+        StartCoroutine(DestroyAfterPlay());
+    }    
+
+    IEnumerator DestroyAfterPlay () {
+        while (particleSystem.isPlaying) {
+            yield return new WaitForSeconds(.1f);
+        }
+
+        Destroy(this.gameObject);
+    }
+}
