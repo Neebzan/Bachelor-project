4eebb60a559c0e4948f9b2b6e13d89277de5c372
Esben Dalsgard <esbenjd@gmail.com>
2020-05-15 23:46:54 +0200

    no message

--- Unity/VR Adventure/Assets/Particle Systems/Fireball Explosion/FireballExplosionParticles.cs
+++ Unity/VR Adventure/Assets/Particle Systems/Fireball Explosion/FireballExplosionParticles.cs
@@ -6,10 +6,15 @@ using UnityEngine;
 public class FireballExplosionParticles : MonoBehaviour
 {
     ParticleSystem particleSystem;
+    public float Size { get; set; }
+
     // Start is called before the first frame update
     void Start()
     {
         particleSystem = GetComponent<ParticleSystem>();
+        //particleSystem.main.
+
+        //particleSystem.shape.radius = Size;
         StartCoroutine(DestroyAfterPlay());
     }    
 
