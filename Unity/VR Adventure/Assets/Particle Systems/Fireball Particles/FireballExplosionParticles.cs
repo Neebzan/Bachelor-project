9df5035104ccd70dbea0d1c061534c3bbd16aede
Esben Dalsgard <esbenjd@gmail.com>
2020-05-16 19:20:09 +0200

    Updated particles

---
+++ Unity/VR Adventure/Assets/Particle Systems/Fireball Particles/FireballExplosionParticles.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballExplosionParticles : MonoBehaviour
+{
+
+    public ParticleSystem TrailParticleSystem;
+    public ParticleSystem SphereParticleSystem;
+    public float Size { get; set; }
+    public Vector3 Velocity { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int numOfParticles = (int)(Size * 100.0f);
+
+        //var velocityParamters = new ParticleSystem.EmitParams() { velocity = Velocity };
+
+        TrailParticleSystem.Emit(numOfParticles);
+        //TrailParticleSystem.transform.localScale = new Vector3(Size, Size, Size);
+        SphereParticleSystem.transform.localScale = new Vector3(Size, Size, Size);
+        SphereParticleSystem.Emit(numOfParticles);
+
+        //particleSystem.shape.radius = Size;
+        StartCoroutine(DestroyAfterPlay());
+    }    
+
+    IEnumerator DestroyAfterPlay () {
+        while (TrailParticleSystem.particleCount > 0 && SphereParticleSystem.particleCount > 0) {
+            yield return new WaitForSeconds(.1f);
+        }
+
+        Destroy(this.gameObject);
+    }
+}
--- Unity/VR Adventure/Assets/Scripts/Networking/Client/EmulatedFireball.cs
+++ Unity/VR Adventure/Assets/Scripts/Networking/Client/EmulatedFireball.cs
@@ -1,34 +1,61 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+
+using System.Threading;
 using UnityEngine;
+using Stopwatch = System.Diagnostics.Stopwatch;
+using UnityEngine.PlayerLoop;
 
 public class EmulatedFireball : MonoBehaviour {
     private Material _material;
     private readonly string _sizeProperty = "Vector1_EE3E2A7D";
     private float _size = 0.0f;
-    public ParticleSystem ParticleSystem;
+    public GameObject ParticleSystem;
     public int ID { get; private set; }
+
+    private Vector3 _oldPos;
+    private Vector3 _newPos;
+    private Vector3 _velocity;
+
+    private DateTime _lastTick;
+    private float _tickTime;
+    private Stopwatch stopwatch = new Stopwatch();
 
     void Awake () {
         _material = GetComponent<MeshRenderer>().material;
-        _material.SetFloat(_sizeProperty, _size);
+        transform.localScale = new Vector3(_size, _size, _size);
+        //_material.SetFloat(_sizeProperty, _size);
     }
 
     public void Init (int id, float size) {
         ID = id;
         _size = size;
+        //_tickTime = 0.0f;
+        //stopwatch.Start();
     }
 
     public void Emulate (Vector3 position, float size) {
+        //_tickTime = stopwatch.ElapsedMilliseconds;
+        //stopwatch.Restart();
+
+        //Debug.Log(_tickTime);
+
+        //_oldPos = transform.position;
         transform.position = position;
+        //_newPos = transform.position;
+        //_velocity = (_newPos - _oldPos) / _tickTime;
+
         _size = size;
-        _material.SetFloat(_sizeProperty, _size);
+
+        transform.localScale = new Vector3(_size, _size, _size);
     }
 
     public void Despawn (bool explode) {
         if (explode) {
-            GameObject.Instantiate(ParticleSystem, transform.position, Quaternion.identity);
+            var particles = GameObject.Instantiate(ParticleSystem, transform.position, Quaternion.identity).GetComponent<FireballExplosionParticles>();
+            particles.Size = _size;
+            //particles.Velocity = _velocity;
             Remove();
         }
         else {
@@ -36,10 +63,12 @@ public class EmulatedFireball : MonoBehaviour {
         }
     }
 
+
     IEnumerator Shrink () {
         while (_size > 0.0f) {
             _size -= Time.fixedDeltaTime * .1f;
-            _material.SetFloat(_sizeProperty, _size);
+            transform.localScale = new Vector3(_size, _size, _size);
+            //_material.SetFloat(_sizeProperty, _size);
             yield return new WaitForFixedUpdate();
         }
 
