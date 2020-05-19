cd51404d0c0ca17c392511fcfc4a977e127c1a7f
Esben Dalsgard <esbenjd@gmail.com>
2020-05-19 11:38:00 +0200

    Forgot to save scene hihi

--- Unity/VR Adventure/Assets/Scripts/Networking/Client/GameManager.cs
+++ Unity/VR Adventure/Assets/Scripts/Networking/Client/GameManager.cs
@@ -3,51 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
     public static GameManager instance;
 
     public Dictionary<int, PlayerManager> EmulatedPlayers = new Dictionary<int, PlayerManager>();
     public Dictionary<int, EmulatedFireball> EmulatedFireballs = new Dictionary<int, EmulatedFireball>();
-    //public static Dictionary<int, Projectile> projectiles = new Dictionary<int, Projectile>();
-
 
     public GameObject localPlayerPrefab;
     public GameObject playerPrefab;
-    public GameObject projectilePrefab;
     public GameObject EmulatedFireballPrefab;
 
-    private void Awake()
-    {
+    private void Awake () {
         if (instance == null)
             instance = this;
-        else if (instance != this)
-        {
+        else if (instance != this) {
             Debug.Log("Instance already exists!");
             Destroy(this);
         }
     }
 
-    public void SpawnPlayer(int _id, string _username, Vector3 _position, Quaternion _rotation)
-    {
+    public void SpawnPlayer (int _id, string _username, Vector3 _position, Quaternion _rotation) {
         GameObject _player;
-        //Check if spawning local player or remote player
-        //if (_id == Client.instance.id)
-        //    _player = Instantiate(localPlayerPrefab, _position, _rotation);
-        //else
-
         _player = Instantiate(playerPrefab, _position, _rotation);
-
         _player.GetComponent<PlayerManager>().Initialize(_id, _username);
-        //_player.GetComponent<PlayerManager>().username = _username;
-
         EmulatedPlayers.Add(_id, _player.GetComponent<PlayerManager>());
 
     }
 
-    public void SpawnEmulatedFireball(int id, Vector3 position, float size) {
+    public void SpawnEmulatedFireball (int id, Vector3 position, float size) {
         EmulatedFireball emulatedFireball = Instantiate(EmulatedFireballPrefab, position, Quaternion.identity).GetComponent<EmulatedFireball>();
         emulatedFireball.Init(id, size);
-       EmulatedFireballs.Add(id, emulatedFireball);
+        EmulatedFireballs.Add(id, emulatedFireball);
     }
 }
