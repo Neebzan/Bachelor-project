392238714499eb9d9f28a047762c0cea7db8899f
Esben Dalsgard <esbenjd@gmail.com>
2020-05-21 13:18:34 +0200

    Added test project for game servers

---
+++ Solutions/K8SGameServerDevelopment/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace K8SGameServerDevelopment {
+    class Program {
+        static void Main (string [ ] args) {
+            Console.WriteLine("Server started");
+            Console.ReadKey();
+        }
+    }
+}
