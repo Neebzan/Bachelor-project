9645c42fbe5f693e264e61e820b3a1df64bd5877
Esben Dalsgard <esbenjd@gmail.com>
2020-05-21 12:04:19 +0200

    Working on K8S game servers

---
+++ Solutions/GameServerDecoy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace GameServerDecoy {
+    class Program {
+        static void Main (string [ ] args) {
+            TcpClient client = new TcpClient("212.10.51.254", 27001);
+            if (client.Connected) {
+                Console.WriteLine("Connected");
+            }
+            else {
+                Console.WriteLine("Not connected");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Solutions/ServerManager/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerManager {
+    class Program {
+        static List<TcpClient> tcpClients = new List<TcpClient>();
+
+        static void Main (string [ ] args) {
+            Console.WriteLine("Started");
+            Console.WriteLine("Listening for connections");
+            ListenForConnections();
+        }
+
+        public static void ListenForConnections () {
+            TcpListener listener = new TcpListener(IPAddress.Any, 27001);
+            listener.Start();
+            while (true) {
+                TcpClient client = listener.AcceptTcpClient();
+                tcpClients.Add(client);
+                Console.WriteLine($"Connected! from {client.Client.RemoteEndPoint.ToString()}");
+                Console.WriteLine("Begin K8S API attempt");
+                Process process = new Process();
+                process.StartInfo = new ProcessStartInfo("kubectl get pods");
+                process.Start();
+            }
+        }
+    }
+
+}
